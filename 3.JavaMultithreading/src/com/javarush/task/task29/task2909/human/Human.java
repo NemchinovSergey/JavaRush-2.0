c03ac4d7cf091ae605887522cbe32d280d8ce326
Sergey Nemchinov <googoodemon@gmail.com>
2017-04-05 15:25:38 +0700

    big task2909 (9)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -5,20 +5,21 @@ import java.util.Collections;
 import java.util.List;
 
 public class Human implements Alive {
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
-
     private static int nextId = 0;
 
     protected int age;
     protected String name;
-    protected int[] size;
+
+    public class Size {
+        public int height;
+        public int weight;
+    }
+
+    protected Size size;
 
     private int id;
     private List<Human> children = new ArrayList<>();
-    private int bloodGroup;
+    private BloodGroup bloodGroup;
 
     public Human(String name, int age) {
         this.name = name;
@@ -27,12 +28,12 @@ public class Human implements Alive {
         nextId++;
     }
 
-    public int getBloodGroup() {
+    public BloodGroup getBloodGroup() {
         return bloodGroup;
     }
 
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
     }
 
     public List<Human> getChildren() {
@@ -71,7 +72,7 @@ public class Human implements Alive {
     }
 
     public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
     }
 
     public String getPosition() {
