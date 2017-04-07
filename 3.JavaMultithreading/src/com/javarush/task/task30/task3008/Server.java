7aca69964de5faddc752917531e1bfaa98896c26
Sergey Nemchinov <googoodemon@gmail.com>
2017-04-07 13:24:11 +0700

    big task3008 (6) Sockets

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,10 +1,50 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by nemchinov on 06.04.2017.
  */
 public class Server {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        //ConsoleHelper.writeMessage("Enter server socket number:");
+        int serverPort = ConsoleHelper.readInt();
+
+        try (ServerSocket serverSocket = new ServerSocket(serverPort)) {
+            ConsoleHelper.writeMessage(String.format("Server started at %d port", serverSocket.getLocalPort()));
+
+            while (!Thread.currentThread().isInterrupted()) {
+                Socket client = serverSocket.accept();
+                Handler handler = new Handler(client);
+                handler.start();
+            }
+
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage(e.getMessage());
+        }
+
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+
+
+                    sleep(10);
+                }
+            } catch (InterruptedException ioException) {
+            }
+        }
     }
 }
