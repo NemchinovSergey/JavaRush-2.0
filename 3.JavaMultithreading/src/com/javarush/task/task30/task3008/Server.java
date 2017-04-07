dbb6802d1a4d39cad4bd54c8cd39487a388705d8
Sergey Nemchinov <googoodemon@gmail.com>
2017-04-07 15:08:40 +0700

    big task3008 (7) Broadcast Message

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,11 +3,25 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by nemchinov on 06.04.2017.
  */
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+    public static void sendBroadcastMessage(Message message) {
+        for (Connection connection : connectionMap.values()) {
+            try {
+                connection.send(message);
+            }
+            catch (IOException e) {
+                ConsoleHelper.writeMessage("Error send message");
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         //ConsoleHelper.writeMessage("Enter server socket number:");
