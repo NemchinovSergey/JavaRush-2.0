7ce990811d3752d0e6c5fe8300a063ad9aed73a9
Sergey Nemchinov <googoodemon@gmail.com>
2017-04-11 14:21:53 +0700

    big task3008 (19) Bot processMessage()

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,5 +1,12 @@
 package com.javarush.task.task30.task3008.client;
 
+import com.javarush.task.task30.task3008.ConsoleHelper;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by nemchinov on 11.04.2017.
  */
@@ -27,6 +34,56 @@ public class BotClient extends Client {
     }
 
     public class BotSocketThread extends Client.SocketThread {
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
 
+        @Override
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+
+            if (message.contains(":")) {
+                int splitterPos = message.indexOf(":");
+                String senderName = message.substring(0, splitterPos);
+                String textMessage = message.substring(splitterPos + 2);
+
+                switch (textMessage) {
+                    case "дата":
+                        sendTextMessage(String.format("Информация для %s: %s", senderName,
+                                new SimpleDateFormat("d.MM.YYYY").format(Calendar.getInstance().getTime())));
+                        break;
+                    case "день":
+                        sendTextMessage(String.format("Информация для %s: %s", senderName,
+                                new SimpleDateFormat("d").format(Calendar.getInstance().getTime())));
+                        break;
+                    case "месяц":
+                        sendTextMessage(String.format("Информация для %s: %s", senderName,
+                                new SimpleDateFormat("MMMM").format(Calendar.getInstance().getTime())));
+                        break;
+                    case "год":
+                        sendTextMessage(String.format("Информация для %s: %s", senderName,
+                                new SimpleDateFormat("YYYY").format(Calendar.getInstance().getTime())));
+                        break;
+                    case "время":
+                        sendTextMessage(String.format("Информация для %s: %s", senderName,
+                                new SimpleDateFormat("H:mm:ss").format(Calendar.getInstance().getTime())));
+                        break;
+                    case "час":
+                        sendTextMessage(String.format("Информация для %s: %s", senderName,
+                                new SimpleDateFormat("H").format(Calendar.getInstance().getTime())));
+                        break;
+                    case "минуты":
+                        sendTextMessage(String.format("Информация для %s: %s", senderName,
+                                new SimpleDateFormat("m").format(Calendar.getInstance().getTime())));
+                        break;
+                    case "секунды":
+                        sendTextMessage(String.format("Информация для %s: %s", senderName,
+                                new SimpleDateFormat("s").format(Calendar.getInstance().getTime())));
+                        break;
+                }
+            }
+        }
     }
 }
