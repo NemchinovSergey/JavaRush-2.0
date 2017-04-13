1ffa3817e22772439fc2a6025b3f087851bd6514
Sergey Nemchinov <googoodemon@gmail.com>
2017-04-13 13:05:23 +0700

    big task3110 (8)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,29 +12,34 @@ import java.nio.file.Paths;
  * Created by nemchinov on 13.04.2017.
  */
 public class Archiver {
-    public static void main(String[] args) {
 
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage( "Выберите операцию:\n" +
+                Operation.CREATE.ordinal()  + " - упаковать файлы в архив\n" +
+                Operation.ADD.ordinal()     + " - добавить файл в архив\n" +
+                Operation.REMOVE.ordinal()  + " - удалить файл из архива\n" +
+                Operation.EXTRACT.ordinal() + " - распаковать архив\n" +
+                Operation.CONTENT.ordinal() + " - просмотреть содержимое архива\n" +
+                Operation.EXIT.ordinal()    + " – выход");
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)))
-        {
-            System.out.println("Enter path to archive: ");
-            String archivePath = reader.readLine();
+        int operationNumber = ConsoleHelper.readInt();
+        return Operation.values()[operationNumber];
+    }
 
-            System.out.println("Enter path to file to zip: ");
-            String filePath = reader.readLine();
-
-            ZipFileManager zipFileManager = new ZipFileManager(Paths.get(archivePath));
-            zipFileManager.createZip(Paths.get(filePath));
-
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static void main(String[] args)
+    {
+        Operation operation = null;
+        while (operation != Operation.EXIT) {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            }
+            catch (WrongZipFileException wrongZipFileExc) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            }
+            catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
         }
-
-        try {
-            new ExitCommand().execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
     }
 }
