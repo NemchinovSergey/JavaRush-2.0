5610231169b66694664599780ae093051b76e059
Sergey Nemchinov <googoodemon@gmail.com>
2017-04-13 17:27:30 +0700

    big task3110 (10)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
@@ -1,16 +1,12 @@
 package com.javarush.task.task31.task3110;
 
 import java.io.IOException;
-import java.nio.file.DirectoryIteratorException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by nemchinov on 13.04.2017.
- */
 public class FileManager {
     private Path rootPath;
     private List<Path> fileList;
@@ -26,18 +22,20 @@ public class FileManager {
     }
 
     private void collectFileList(Path path) throws IOException {
+        // Добавляем только файлы
         if (Files.isRegularFile(path)) {
-            fileList.add(rootPath.relativize(path));
+            Path relativePath = rootPath.relativize(path);
+            fileList.add(relativePath);
         }
-        else if (Files.isDirectory(path)) {
+
+        // Добавляем содержимое директории
+        if (Files.isDirectory(path)) {
+            // Рекурсивно проходимся по всему содержмому директории
+            // Чтобы не писать код по вызову close для DirectoryStream, обернем вызов newDirectoryStream в try-with-resources
             try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
-                for (Path entry: directoryStream) {
-                    collectFileList(entry);
+                for (Path file : directoryStream) {
+                    collectFileList(file);
                 }
-            }
-            catch (DirectoryIteratorException ex) {
-                // I/O error encounted during the iteration, the cause is an IOException
-                throw ex.getCause();
             }
         }
     }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,8 +1,12 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -17,21 +21,47 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        // Проверяем, существует ли директория, где будет создаваться архив
+        // При необходимости создаем ее
+        Path zipDirectory = zipFile.getParent();
+        if (Files.notExists(zipDirectory)) {
+            Files.createDirectories(zipDirectory);
+        }
+
         try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
-
-            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
-            zipOutputStream.putNextEntry(zipEntry);
-
-            try (InputStream inputStream = Files.newInputStream(source)) {
-                byte[] buff = new byte[1024];
-
-                while (inputStream.available() > 0) {
-                    int count = inputStream.read(buff);
-                    zipOutputStream.write(buff, 0, count);
+            if (Files.isRegularFile(source)) {
+                // Если архивируем отдельный файл, то нужно получить его директорию и имя
+                addNewZipEntry(zipOutputStream, source.getParent(), source.getFileName());
+            }
+            else if (Files.isDirectory(source)) {
+                FileManager fileManager = new FileManager(source);
+                List<Path> fileNames = fileManager.getFileList();
+                for (Path fileName : fileNames) {
+                    addNewZipEntry(zipOutputStream, source, fileName);
                 }
             }
+            else {
+                // Если переданный source не директория и не файл, бросаем исключение
+                throw new PathIsNotFoundException();
+            }
+        }
+    }
 
+    private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
+        Path fullPath = filePath.resolve(fileName);
+        try (InputStream inputStream = Files.newInputStream(fullPath)) {
+            ZipEntry zipEntry = new ZipEntry(fileName.toString());
+            zipOutputStream.putNextEntry(zipEntry);
+            copyData(inputStream, zipOutputStream);
             zipOutputStream.closeEntry();
+        }
+    }
+
+    private void copyData(InputStream in, OutputStream out) throws Exception {
+        byte[] buff = new byte[1024 * 1024];
+        while (in.available() > 0) {
+            int count = in.read(buff);
+            out.write(buff, 0, count);
         }
     }
 }
