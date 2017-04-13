728721cb9f4bd1c4da09837579f76773769f2897
Sergey Nemchinov <googoodemon@gmail.com>
2017-04-13 18:05:48 +0700

    big task3110 (11)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -10,11 +10,9 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-/**
- * Created by nemchinov on 13.04.2017.
- */
 public class ZipFileManager {
-    private Path zipFile;
+    // Полный путь zip файла
+    private final Path zipFile;
 
     public ZipFileManager(Path zipFile) {
         this.zipFile = zipFile;
@@ -24,23 +22,27 @@ public class ZipFileManager {
         // Проверяем, существует ли директория, где будет создаваться архив
         // При необходимости создаем ее
         Path zipDirectory = zipFile.getParent();
-        if (Files.notExists(zipDirectory)) {
+        if (Files.notExists(zipDirectory))
             Files.createDirectories(zipDirectory);
-        }
 
+        // Создаем zip поток
         try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
-            if (Files.isRegularFile(source)) {
-                // Если архивируем отдельный файл, то нужно получить его директорию и имя
-                addNewZipEntry(zipOutputStream, source.getParent(), source.getFileName());
-            }
-            else if (Files.isDirectory(source)) {
+
+            if (Files.isDirectory(source)) {
+                // Если архивируем директорию, то нужно получить список файлов в ней
                 FileManager fileManager = new FileManager(source);
                 List<Path> fileNames = fileManager.getFileList();
-                for (Path fileName : fileNames) {
+
+                // Добавляем каждый файл в архив
+                for (Path fileName : fileNames)
                     addNewZipEntry(zipOutputStream, source, fileName);
-                }
-            }
-            else {
+
+            } else if (Files.isRegularFile(source)) {
+
+                // Если архивируем отдельный файл, то нужно получить его директорию и имя
+                addNewZipEntry(zipOutputStream, source.getParent(), source.getFileName());
+            } else {
+
                 // Если переданный source не директория и не файл, бросаем исключение
                 throw new PathIsNotFoundException();
             }
@@ -50,18 +52,21 @@ public class ZipFileManager {
     private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
         Path fullPath = filePath.resolve(fileName);
         try (InputStream inputStream = Files.newInputStream(fullPath)) {
-            ZipEntry zipEntry = new ZipEntry(fileName.toString());
-            zipOutputStream.putNextEntry(zipEntry);
+            ZipEntry entry = new ZipEntry(fileName.toString());
+
+            zipOutputStream.putNextEntry(entry);
+
             copyData(inputStream, zipOutputStream);
+
             zipOutputStream.closeEntry();
         }
     }
 
     private void copyData(InputStream in, OutputStream out) throws Exception {
-        byte[] buff = new byte[1024 * 1024];
-        while (in.available() > 0) {
-            int count = in.read(buff);
-            out.write(buff, 0, count);
+        byte[] buffer = new byte[8 * 1024];
+        int len;
+        while ((len = in.read(buffer)) > 0) {
+            out.write(buffer, 0, len);
         }
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,7 +1,17 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Paths;
+
 /**
  * Created by nemchinov on 13.04.2017.
  */
 public abstract class ZipCommand implements Command {
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь для создаваемого файла-архива: ");
+        String zipFileName = ConsoleHelper.readString();
+        return new ZipFileManager(Paths.get(zipFileName));
+    }
 }
