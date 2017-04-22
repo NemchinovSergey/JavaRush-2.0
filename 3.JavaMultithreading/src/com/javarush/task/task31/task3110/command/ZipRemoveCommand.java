15b91c247a2e4da4fbf91fa103afa3ea1fcbceb4
Nemchinov Sergey <googoodemon@gmail.com>
2017-04-22 18:42:43 +0700

    big task3110 (17)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
@@ -2,30 +2,21 @@ package com.javarush.task.task31.task3110.command;
 
 import com.javarush.task.task31.task3110.ConsoleHelper;
 import com.javarush.task.task31.task3110.ZipFileManager;
-import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-/**
- * Created by nemchinov on 13.04.2017.
- */
 public class ZipRemoveCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-        try {
-            ConsoleHelper.writeMessage("Удаление файлов из архива");
+        ConsoleHelper.writeMessage("Удаление файла из архива.");
 
-            ZipFileManager zipFileManager = getZipFileManager();
+        ZipFileManager zipFileManager = getZipFileManager();
 
-            ConsoleHelper.writeMessage("Введите мя файла для удаления:");
-            String fileName = ConsoleHelper.readString();
-            zipFileManager.removeFile(Paths.get(fileName));
+        ConsoleHelper.writeMessage("Введите полный путь файла в архиве:");
+        Path sourcePath = Paths.get(ConsoleHelper.readString());
+        zipFileManager.removeFile(sourcePath);
 
-            ConsoleHelper.writeMessage("Файл удалён");
-
-        } catch (PathIsNotFoundException e) {
-            ConsoleHelper.writeMessage("Неверный путь для распаковки.");
-        }
+        ConsoleHelper.writeMessage("Удаление из архива завершено.");
     }
 }
