3c17f2a7a36906fe3a471c87749ee74432646c42
Nemchinov Sergey <googoodemon@gmail.com>
2017-04-25 21:12:56 +0700

    bigtask3209 "HTML Editor" (17)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,11 +1,10 @@
 package com.javarush.task.task32.task3209;
 
-import javax.swing.text.BadLocationException;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
-import java.io.IOException;
 import java.io.StringReader;
+import java.io.StringWriter;
 
 public class Controller {
     private View view;
@@ -52,8 +51,18 @@ public class Controller {
         StringReader reader = new StringReader(text);
         try {
             new HTMLEditorKit().read(reader, document, 0);
-        } catch (IOException | BadLocationException e) {
+        } catch (Exception e) {
             ExceptionHandler.log(e);
         }
+    }
+
+    public String getPlainText() {
+        StringWriter writer = new StringWriter();
+        try {
+            new HTMLEditorKit().write(writer, document, 0, document.getLength());
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+        return writer.toString();
     }
 }
