70b4ff73ef8b309c442d2b56a2ccb234a1056008
Nemchinov Sergey <googoodemon@gmail.com>
2017-04-25 21:32:36 +0700

    bigtask3209 "HTML Editor" (18)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -80,7 +80,11 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void selectedTabChanged() {
-
+        switch (tabbedPane.getSelectedIndex()) {
+            case 0: controller.setPlainText(plainTextPane.getText()); break;
+            case 1: plainTextPane.setText(controller.getPlainText());
+        }
+        resetUndo();
     }
 
     public boolean canUndo() {
