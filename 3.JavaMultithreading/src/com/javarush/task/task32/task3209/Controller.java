5605af2302290d1931585851e075f4b3335f9b98
Nemchinov Sergey <googoodemon@gmail.com>
2017-04-26 09:50:20 +0700

    bigtask3209 "HTML Editor" (22)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.*;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
 
@@ -15,16 +17,16 @@ public class Controller {
         this.view = view;
     }
 
-    public void init() {
-        createNewDocument();
-    }
-
     public static void main(String[] args) {
         View view = new View();
         Controller controller = new Controller(view);
         view.setController(controller);
         view.init();
         controller.init();
+    }
+
+    public void init() {
+        createNewDocument();
     }
 
     public void exit() {
@@ -46,16 +48,6 @@ public class Controller {
         view.update();
     }
 
-    public void setPlainText(String text) {
-        resetDocument();
-        StringReader reader = new StringReader(text);
-        try {
-            new HTMLEditorKit().read(reader, document, 0);
-        } catch (Exception e) {
-            ExceptionHandler.log(e);
-        }
-    }
-
     public String getPlainText() {
         StringWriter writer = new StringWriter();
         try {
@@ -64,6 +56,16 @@ public class Controller {
             ExceptionHandler.log(e);
         }
         return writer.toString();
+    }
+
+    public void setPlainText(String text) {
+        resetDocument();
+        StringReader reader = new StringReader(text);
+        try {
+            new HTMLEditorKit().read(reader, document, 0);
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
     }
 
     public void createNewDocument() {
@@ -83,6 +85,21 @@ public class Controller {
     }
 
     public void saveDocumentAs() {
+        view.selectHtmlTab();
 
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new HTMLFileFilter());
+
+        if (fileChooser.showSaveDialog(view) == JFileChooser.APPROVE_OPTION) {
+            currentFile = fileChooser.getSelectedFile();
+            view.setTitle(currentFile.getName());
+
+            try (FileWriter writer = new FileWriter(currentFile)) {
+                new HTMLEditorKit().write(writer, document, 0, document.getLength());
+            } catch (Exception e) {
+                ExceptionHandler.log(e);
+            }
+
+        }
     }
 }
