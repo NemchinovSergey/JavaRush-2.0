9eef67868dac99d6e7beee29b30bddb9ca25d783
Sergey Nemchinov <googoodemon@gmail.com>
2017-05-23 10:21:23 +0700

    bigtask3707 (1) My own set - class AmigoSet

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task37.task3707;
+
+import java.io.Serializable;
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class AmigoSet<E> extends AbstractSet implements Set, Serializable, Cloneable {
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
