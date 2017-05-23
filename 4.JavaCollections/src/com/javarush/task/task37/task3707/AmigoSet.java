d3a1c1045821783f7647bef674b49870b6edab34
Sergey Nemchinov <googoodemon@gmail.com>
2017-05-23 11:09:22 +0700

    bigtask3707 (2) My own set - class AmigoSet

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,23 +1,45 @@
 package com.javarush.task.task37.task3707;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
-public class AmigoSet<E> extends AbstractSet implements Set, Serializable, Cloneable {
-    @Override
-    public Iterator iterator() {
-        return null;
+public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Serializable, Cloneable {
+    private final static Object PRESENT = new Object();
+    private transient HashMap<E, Object> map;
+
+    public AmigoSet() {
+        this.map = new HashMap<>();
+    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        int capacity = Math.max((int)(collection.size() / 0.75f + 1), 16);
+        this.map = new HashMap<>(capacity);
+        addAll(collection);
+    }
+
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
     }
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
     }
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
+    }
+
+    @Override
+    public boolean add(E e) {
+        if (map.containsKey(e)) return false;
+        map.put(e, PRESENT);
+        return true;
     }
 }
