b65930cc04ba9e4b34223411f15a4af5a9ab31da
Sergey Nemchinov <googoodemon@gmail.com>
2017-07-06 10:22:23 +0700

    task0712 finished

--- 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task07.task0712;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
@@ -12,5 +11,38 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        ArrayList<String> list = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            for (int i = 0; i < 10; i++) {
+                list.add(reader.readLine());
+            }
+        }
+
+        int minLength = Integer.MAX_VALUE;
+        int minIndex = 0;
+        int maxLength = Integer.MIN_VALUE;
+        int maxIndex = 0;
+
+        for (int i = 0; i < 10; i++) {
+            String str = list.get(i);
+
+            if (str.length() < minLength) {
+                minLength = str.length();
+                minIndex = i;
+            }
+
+            if (str.length() > maxLength) {
+                maxLength = str.length();
+                maxIndex = i;
+            }
+        }
+
+        if (minIndex < maxIndex) {
+            System.out.println(list.get(minIndex));
+        }
+        else {
+            System.out.println(list.get(maxIndex));
+        }
     }
 }
