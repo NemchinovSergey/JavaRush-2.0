1d55b8507c4e131d03c7e00bd8a0abeab7ad3565
Sergey Nemchinov <googoodemon@gmail.com>
2017-07-06 09:45:33 +0700

    task0706 finished

--- 1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task07.task0706;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 /* 
@@ -11,5 +10,33 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        int[] numbers = new int[15];
+        int sumOdd = 0;
+        int sumEven = 0;
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            for (int i = 0; i < numbers.length; i++) {
+                numbers[i] = Integer.parseInt(reader.readLine());
+            }
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if ( i == 0 || i % 2 == 0) {
+                sumEven += numbers[i];
+            }
+            else {
+                sumOdd += numbers[i];
+            }
+        }
+
+        if (sumOdd > sumEven) {
+            System.out.println("В домах с нечетными номерами проживает больше жителей.");
+        }
+        else if (sumOdd < sumEven) {
+            System.out.println("В домах с четными номерами проживает больше жителей.");
+        }
+        else {
+            System.out.println("В домах с четными и нечетными номерами проживает одинаковое количество жителей.");
+        }
     }
 }
