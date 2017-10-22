8355767b5ade8b06489de7a8b86e118bd7bd73db
Sergey Nemchinov <googoodemon@gmail.com>
2017-10-22 14:31:24 +0700

    task0812

--- 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Objects;
 
 /* 
 Cамая длинная последовательность
@@ -20,6 +21,26 @@ public class Solution {
             }
         }
 
+        int x = integers.get(0);
+        int count = 1;
+        int max = 1;
 
+        for (int i = 1; i < integers.size(); i++) {
+            // Если следующее число повторяется, то увеличиваем счетчик
+            if (Objects.equals(integers.get(i), x)) {
+                count++;
+            }
+            else {
+                // Если число другое, то начинаем подсчёт заново
+                x = integers.get(i);
+                count = 1;
+            }
+            // запоминаем максимальное число повторений
+            if (count > max) {
+                max = count;
+            }
+        }
+
+        System.out.println(max);
     }
 }
