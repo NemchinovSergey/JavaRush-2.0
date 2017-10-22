d65e3e478486335e635b50437a781ad730dbc224
Sergey Nemchinov <googoodemon@gmail.com>
2017-10-22 15:00:55 +0700

    task0818 by iterator

--- 1.JavaSyntax/src/com/javarush/task/task08/task0818/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0818/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task08.task0818;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /* 
 Только для богачей
@@ -8,14 +10,37 @@ import java.util.HashMap;
 
 public class Solution {
     public static HashMap<String, Integer> createMap() {
-        //напишите тут ваш код
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Kuznetsov", 900);
+        map.put("Hitrov", 1000);
+        map.put("Ivanov", 100);
+        map.put("Petrov", 200);
+        map.put("Sidorov", 300);
+        map.put("Lisitsin", 800);
+        map.put("Zayzev", 400);
+        map.put("Volkov", 500);
+        map.put("Medvedev", 600);
+        map.put("Orlov", 700);
+        return map;
     }
 
     public static void removeItemFromMap(HashMap<String, Integer> map) {
-        //напишите тут ваш код
+        for (Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator(); it.hasNext(); ) {
+            Map.Entry<String, Integer> entry = it.next();
+            if (entry.getValue() < 500) {
+                it.remove();
+            }
+        }
     }
 
     public static void main(String[] args) {
+        HashMap<String, Integer> map = createMap();
 
+        //map.forEach((s, integer) -> System.out.println(String.format("%s: %d", s, integer)));
+
+        removeItemFromMap(map);
+
+        //System.out.println("After removing:");
+        //map.forEach((s, integer) -> System.out.println(String.format("%s: %d", s, integer)));
     }
 }
