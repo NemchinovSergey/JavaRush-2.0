2ca5bb367cedc7bed65b9c7f0684aa28e9bd01ee
Sergey Nemchinov <googoodemon@gmail.com>
2017-10-29 19:45:34 +0700

    task3613 is done

--- 4.JavaCollections/src/com/javarush/task/task36/task3613/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3613/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task36.task3613;
 
+import java.util.concurrent.SynchronousQueue;
 
 /* 
 Найти класс по описанию
@@ -10,6 +11,6 @@ public class Solution {
     }
 
     public static Class getExpectedClass() {
-        return null;
+        return SynchronousQueue.class;
     }
 }
