cedcb39149e5bfd95c7fffa65565b800cd7477a7
Sergey Nemchinov <googoodemon@gmail.com>
2017-11-05 19:47:07 +0700

    task3610

--- 4.JavaCollections/src/com/javarush/task/task36/task3610/MyMultiMap.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3610/MyMultiMap.java
@@ -15,37 +15,83 @@ public class MyMultiMap<K, V> extends HashMap<K, V> implements Cloneable, Serial
 
     @Override
     public int size() {
-        //напишите тут ваш код
+        final int[] size = {0};
+        map.values().forEach(vs -> size[0] += vs.size());
+        return size[0];
     }
 
     @Override
     public V put(K key, V value) {
-        //напишите тут ваш код
+        List<V> list;
+
+        if (containsKey(key)) {
+            // Получаем список по ключу
+            list = map.get(key);
+            if (list == null) {
+                throw new IllegalStateException();
+            }
+            V prev = null;
+            if (list.size() > 0) {
+                prev = list.get(list.size() - 1);
+            }
+            if (list.size() >= repeatCount) {
+                list.remove(0);
+            }
+            list.add(value);
+            return prev;
+        }
+        else {
+            // Если списка нет, то создаём его и добавляем список в мапу
+            list = new ArrayList<>(repeatCount);
+            list.add(value);
+            map.put(key, list);
+            return null;
+        }
     }
 
     @Override
     public V remove(Object key) {
-        //напишите тут ваш код
+        if (!map.containsKey(key)) {
+            return null;
+        }
+
+        V prev = null;
+        List<V> list = map.get(key);
+        if (list != null) {
+            if (list.size() > 0) {
+                prev = list.remove(0);
+            }
+            if (list.size() == 0) {
+                map.remove(key);
+            }
+            return prev;
+        }
+        else {
+            return null;
+        }
     }
 
     @Override
     public Set<K> keySet() {
-        //напишите тут ваш код
+        return map.keySet();
     }
 
     @Override
     public Collection<V> values() {
         //напишите тут ваш код
+        List<V> list = new ArrayList<>();
+        map.values().forEach(list::addAll);
+        return list;
     }
 
     @Override
     public boolean containsKey(Object key) {
-        //напишите тут ваш код
+        return map.containsKey(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
-        //напишите тут ваш код
+        return values().contains(value);
     }
 
     @Override
