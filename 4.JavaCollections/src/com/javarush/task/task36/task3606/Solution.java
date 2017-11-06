185fccd7580b524c697d7bae759ac712de929182
Sergey Nemchinov <googoodemon@gmail.com>
2017-11-06 20:45:14 +0700

    task3606 done

--- 4.JavaCollections/src/com/javarush/task/task36/task3606/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3606/Solution.java
@@ -1,5 +1,12 @@
 package com.javarush.task.task36.task3606;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,11 +29,88 @@ public class Solution {
         System.out.println(solution.getHiddenClassObjectByKey("packa"));
     }
 
-    public void scanFileSystem() throws ClassNotFoundException {
+    public void scanFileSystem() {
+        File[] files = Paths.get(packageName).toFile().listFiles();
+        if (files == null) {
+            return;
+        }
+
+        ClassLoader myClassLoader = new ClassLoader() {
+            @Override
+            protected Class<?> findClass(String name) throws ClassNotFoundException {
+                try {
+                    byte[] bytes = Files.readAllBytes(Paths.get(name));
+                    return defineClass(null, bytes, 0, bytes.length);
+                } catch (IOException ex) {
+                    return super.findClass(name);
+                }
+            }
+        };
+
+        for (File f : files) {
+            if (isJavaClassFile(f)) {
+                try {
+                    Class clazz = myClassLoader.loadClass(f.getAbsolutePath());
+                    hiddenClasses.add(clazz);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 
     public HiddenClass getHiddenClassObjectByKey(String key) {
+        try {
+            for (Class clazz : hiddenClasses) {
+                if (clazz.getSimpleName().toLowerCase().startsWith(key.toLowerCase())) {
+                    if (isImplementsHiddenClass(clazz)) {
+                        Constructor defaultConstructor = getDefaultConstructor(clazz);
+                        if (defaultConstructor != null) {
+                            return (HiddenClass) defaultConstructor.newInstance();
+                        }
+                    }
+                }
+            }
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
         return null;
     }
+
+    private static boolean isJavaClassFile(File file) {
+        return file.isFile() && file.getName().toLowerCase().endsWith(".class");
+    }
+
+    private static Constructor getDefaultConstructor(Class clazz) {
+        Constructor[] constructors = clazz.getDeclaredConstructors();
+        for (Constructor c : constructors) {
+            if (c.getParameterCount() == 0) {
+                c.setAccessible(true);
+                return c;
+            }
+        }
+        return null;
+    }
+
+    private static boolean isImplementsHiddenClass(Class clazz) {
+        return HiddenClass.class.isAssignableFrom(clazz);
+    }
+
+    //region Not used methods
+    private static boolean hasPublicConstructor(Class clazz) {
+        Constructor[] constructors = clazz.getDeclaredConstructors();
+        for (Constructor c : constructors) {
+            if (c.getParameterCount() == 0 && Modifier.isPublic(c.getModifiers())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static String getClassName(File file) {
+        return file.getName().replace(".class", "");
+    }
+    //endregion
 }
 
