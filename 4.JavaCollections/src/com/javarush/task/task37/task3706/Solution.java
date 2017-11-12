a0191d6497c154c6d7500b288cb1ce7ffe9a57c1
Sergey Nemchinov <googoodemon@gmail.com>
2017-11-12 21:24:34 +0700

    task3706 done

--- 4.JavaCollections/src/com/javarush/task/task37/task3706/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3706/Solution.java
@@ -8,13 +8,25 @@ import java.util.List;
 */
 public class Solution {
     public static void main(String[] args) {
-        List<Number> numbers = Arrays.<Number>asList(1, 2, 3);
+        List<Number> numbers = Arrays.asList(1, 2, 3);
         addDataToList(numbers, getData());
         System.out.println(numbers);
     }
 
+    /** Возвращаём только пустой массив!
+     *
+     *  Если в массиве будет хоть один элемент, то будет попытка вставить данные в методе addDataToList()
+     *  в объект list. Однако он создан с помощью Arrays.asList().
+     *  И в этом вся соль задания!
+     *  Метод Arrays.asList() возвращает неизменяемую обёртку массива, фиксированного размера!
+     *  Если нужно создать нормальный, изменяемый список, то нужно сделать так:
+     *
+     *  List<Number> numbers = new ArrayList<>(Arrays.asList(1, 2, 3));
+     *
+     * @return Объект массив
+     */
     public static Number[] getData() {
-        return null;
+        return new Number[] { };
     }
 
     public static void addDataToList(List<Number> list, Number... data) {
