b9e0e12800eaf0432d146450bf7bf8137123e9a6
Sergey Nemchinov <googoodemon@gmail.com>
2017-11-18 21:05:30 +0700

    task3701 done

--- 4.JavaCollections/src/com/javarush/task/task37/task3701/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3701/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task37.task3701;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.function.Consumer;
 
 /* 
 Круговой итератор
@@ -23,6 +25,49 @@ public class Solution<T> extends ArrayList<T> {
         }
     }
 
-    public class RoundIterator {
+    @Override
+    public Iterator<T> iterator() {
+        return new RoundIterator();
+    }
+
+    public class RoundIterator implements Iterator<T>{
+        // создаём реальный итератор
+        Iterator<T> iterator = Solution.super.iterator();
+
+        @Override
+        public boolean hasNext() {
+            // Если список не пустой, то у нашего кругового итератора
+            // всегда есть следующий элемент
+            return size() > 0;
+        }
+
+        @Override
+        public T next() {
+            // если список не пустой
+            if (hasNext()) {
+                // и есть следующий элемент, то возращаем его
+                if (iterator.hasNext()) {
+                    return iterator.next();
+                }
+                else {
+                    // если дошли до конца, то создаём новый итератор
+                    iterator = Solution.super.iterator();
+                    // и возвращаем первый элемент
+                    return iterator.next();
+                }
+            }
+            else
+                return null; // список пустой - возвращаем null
+        }
+
+        @Override
+        public void remove() {
+            iterator.remove();
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super T> action) {
+            iterator.forEachRemaining(action);
+        }
     }
 }
