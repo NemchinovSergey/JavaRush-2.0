6e2bdc6d3e295c29f85b6c4857e2970770d05985
Sergey Nemchinov <googoodemon@gmail.com>
2017-11-19 21:47:53 +0700

    task3803 done

--- 4.JavaCollections/src/com/javarush/task/task38/task3803/VeryComplexClass.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3803/VeryComplexClass.java
@@ -4,16 +4,31 @@ package com.javarush.task.task38.task3803;
 Runtime исключения (unchecked exception)
 */
 
+import java.util.List;
+
 public class VeryComplexClass {
     public void methodThrowsClassCastException() {
-
+        Object someObject = "a string value";
+        Integer number = (Integer) someObject * 10;
     }
 
     public void methodThrowsNullPointerException() {
-
+        List<String> nullList = null;
+        System.out.println(nullList.size());
     }
 
     public static void main(String[] args) {
+        VeryComplexClass veryComplexClass = new VeryComplexClass();
+        try {
+            veryComplexClass.methodThrowsClassCastException();
+        } catch (ClassCastException e) {
+            e.printStackTrace();
+        }
 
+        try {
+            veryComplexClass.methodThrowsNullPointerException();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }
