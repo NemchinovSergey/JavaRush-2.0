6da853b92090b3b1f5b3fd1ae5c0550327892359
Sergey Nemchinov <googoodemon@gmail.com>
2017-11-19 21:25:02 +0700

    task3802 done

--- 4.JavaCollections/src/com/javarush/task/task38/task3802/VeryComplexClass.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3802/VeryComplexClass.java
@@ -4,12 +4,14 @@ package com.javarush.task.task38.task3802;
 Проверяемые исключения (checked exception)
 */
 
+import java.io.FileInputStream;
+
 public class VeryComplexClass {
     public void veryComplexMethod() throws Exception {
-        //напишите тут ваш код
+        new FileInputStream("file which not exists");
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        new VeryComplexClass().veryComplexMethod();
     }
 }
