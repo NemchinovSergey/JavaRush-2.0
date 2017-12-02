f69c6f95dc3d1a18bdd56be1a4fb491c93d39fb9
Sergey Nemchinov <googoodemon@gmail.com>
2017-12-02 20:41:29 +0700

    task3901 done

--- 4.JavaCollections/src/com/javarush/task/task39/task3901/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3901/Solution.java
@@ -3,6 +3,8 @@ package com.javarush.task.task39.task3901;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 /* 
 Уникальные подстроки
@@ -17,6 +19,29 @@ public class Solution {
     }
 
     public static int lengthOfLongestUniqueSubstring(String s) {
-        return -1;
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+
+        int max = 0;
+        Set<Character> set = new HashSet<>();
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            for (int j = i; j < charArray.length; j++) {
+                char c = charArray[j];
+                if (!set.contains(c)) {
+                    set.add(c);
+                } else {
+                    if (set.size() > max) {
+                        max = set.size();
+                    }
+                    set = new HashSet<>();
+                }
+            }
+        }
+        if (set.size() > max) {
+            max = set.size();
+        }
+        return max;
     }
 }
