6e1516576ab57335a4984b5030b08a686179dbe0
Sergey Nemchinov <googoodemon@gmail.com>
2017-12-09 19:44:58 +0700

    task3905 done

--- 4.JavaCollections/src/com/javarush/task/task39/task3905/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3905/Solution.java
@@ -6,6 +6,32 @@ package com.javarush.task.task39.task3905;
 
 public class Solution {
     public static void main(String[] args) {
+        PhotoPaint photoPaint = new PhotoPaint();
 
+        Color[][] image = {{Color.GREEN,  Color.YELLOW, Color.YELLOW},
+                           {Color.YELLOW, Color.YELLOW, Color.YELLOW},
+                           {Color.YELLOW, Color.YELLOW, Color.BLUE}};
+
+        System.out.println("Before");
+
+        //Show
+        for (Color[] anImage : image) {
+            for (int j = 0; j < image[0].length; j++) {
+                System.out.print(anImage[j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        photoPaint.paintFill(image, 1, 1, Color.RED);
+        System.out.println("After");
+
+        //Show
+        for (Color[] anImage : image) {
+            for (int j = 0; j < image[0].length; j++) {
+                System.out.print(anImage[j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
