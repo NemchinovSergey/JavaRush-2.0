7c9f4ba8f272ccdf21e8368cd117f4894d933d33
Sergey Nemchinov <googoodemon@gmail.com>
2017-12-09 21:09:57 +0700

    task4001 done

--- 4.JavaCollections/src/com/javarush/task/task40/task4001/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4001/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task40.task4001;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -12,14 +13,19 @@ POST, а не GET
 public class Solution {
     public static void main(String[] args) throws Exception {
         Solution solution = new Solution();
-        solution.sendPost(new URL("http://requestb.in/1cse9qt1"), "name=zapp&mood=good&locale=&id=777");
+        solution.sendPost(new URL("https://requestb.in/10wwhyj1"), "name=zapp&mood=good&locale=&id=777");
     }
 
     public void sendPost(URL url, String urlParameters) throws Exception {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        connection.setRequestMethod("GET");
+        connection.setRequestMethod("POST");
         connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+        connection.setDoOutput(true);
+
+        OutputStream outputStream = connection.getOutputStream();
+        outputStream.write(urlParameters.getBytes());
+        outputStream.close();
 
         int responseCode = connection.getResponseCode();
         System.out.println("Response Code: " + responseCode);
