519054ed597106f048f18057dae008cb2ddbcfd6
Sergey Nemchinov <googoodemon@gmail.com>
2017-12-16 18:16:30 +0700

    task3411 done

--- 4.JavaCollections/src/com/javarush/task/task34/task3411/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3411/Solution.java
@@ -11,6 +11,21 @@ public class Solution {
     }
 
     public static void moveRing(char a, char b, char c, int count) {
-        //напишите тут ваш код
+        if (count > 2) {
+            moveRing(a, c, b, count - 1);
+            System.out.println("from " + a + " to " + b);
+            moveRing(c, b, a, count - 1);
+        }
+
+        if (count == 2) {
+            System.out.println("from " + a + " to " + c);
+            System.out.println("from " + a + " to " + b);
+            System.out.println("from " + c + " to " + b);
+            return;
+        }
+
+        if (count == 1) {
+            System.out.println("from " + a + " to " + b);
+        }
     }
 }
