a5ed804c729a09a0ec3be724f063cc70f02a8617
Sergey Nemchinov <googoodemon@gmail.com>
2017-12-16 20:15:25 +0700

    task4006 done

--- 4.JavaCollections/src/com/javarush/task/task40/task4006/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4006/Solution.java
@@ -3,7 +3,8 @@ package com.javarush.task.task40.task4006;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.net.URL;
 
 /* 
@@ -17,17 +18,27 @@ public class Solution {
 
     public static void getSite(URL url) {
         try {
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            Socket socket = new Socket(url.getHost(), url.getDefaultPort());
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
 
-            connection.setRequestMethod("GET");
+            out.println(String.format("GET %s HTTP/1.1", url.getFile()));
+            out.println("Host: " + url.getHost());
+            out.println("User-Agent: Mozilla/5.0");
+            //out.println("Accept: text/html");
+            //out.println("Accept-Language: ru-RU,ru;");
+            //out.println("Accept-Encoding: gzip, deflate");
+            //out.println("Connection: close");
+            out.println();
+            out.flush();
 
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String responseLine;
 
             while ((responseLine = bufferedReader.readLine()) != null) {
                 System.out.println(responseLine);
             }
             bufferedReader.close();
+            out.close();
 
         } catch (IOException e) {
             e.printStackTrace();
