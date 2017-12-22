0196864c436177f452acc820766d69744e0e7772
Sergey Nemchinov <googoodemon@gmail.com>
2017-12-23 00:31:54 +0700

    task2613 (7) done

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/CommandExecutor.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task26.task2613.command;
+
+import com.javarush.task.task26.task2613.Operation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandExecutor {
+    private static final Map<Operation, Command> allKnownCommandsMap;
+    static {
+        allKnownCommandsMap = new HashMap<>();
+        allKnownCommandsMap.put(Operation.DEPOSIT, new DepositCommand());
+        allKnownCommandsMap.put(Operation.INFO, new InfoCommand());
+        allKnownCommandsMap.put(Operation.WITHDRAW, new WithdrawCommand());
+        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
+    }
+
+    private CommandExecutor() {}
+
+    public static final void execute(Operation operation) {
+        Command command = allKnownCommandsMap.get(operation);
+        if (command != null) {
+            command.execute();
+        }
+    }
+}
