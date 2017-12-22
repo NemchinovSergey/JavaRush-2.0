2e0f6665b0629cbbcce97df494e9b1879223a66b
Sergey Nemchinov <googoodemon@gmail.com>
2017-12-22 23:30:34 +0700

    task2810 (5) done

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -1,13 +1,16 @@
 package com.javarush.task.task28.task2810;
 
 import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Controller {
     private Provider[] providers;
 
-    public Controller(Provider... providers) {
+    public Controller(Provider ... providers) {
         if (providers == null || providers.length == 0) {
             throw new IllegalArgumentException();
         }
@@ -19,5 +22,17 @@ public class Controller {
         return "Controller{" +
                 "providers=" + Arrays.toString(providers) +
                 '}';
+    }
+
+    public void scan() {
+        List<Vacancy> vacancies = new ArrayList<Vacancy>();
+        try {
+            for (Provider provider : providers) {
+                vacancies.addAll(provider.getJavaVacancies("java"));
+            }
+            System.out.println(vacancies.size());
+        }
+        catch (NullPointerException ignore) {
+        }
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,8 +1,19 @@
 package com.javarush.task.task28.task2810.model;
 
+import com.javarush.task.task28.task2810.vo.Vacancy;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**  Парсит http://hh.ru
      https://novosibirsk.hh.ru/search/vacancy?text=java+Новосибирск
      https://hh.ru/search/vacancy?text=java+ADDITIONAL_VALUE&page=PAGE_VALUE */
 public class HHStrategy implements Strategy {
     private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
+
+    @Override
+    public List<Vacancy> getVacancies(String searchString) {
+        ArrayList<Vacancy> res = new ArrayList<>();
+        return res;
+    }
 }
