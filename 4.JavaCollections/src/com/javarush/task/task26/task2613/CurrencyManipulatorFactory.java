3516c8958389cea2749a62c33129384af452e736
Sergey Nemchinov <googoodemon@gmail.com>
2017-12-23 00:20:38 +0700

    task2613 (3) done

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulatorFactory.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task26.task2613;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CurrencyManipulatorFactory {
+    private static Map<String, CurrencyManipulator> map = new HashMap<String, CurrencyManipulator>();
+
+    public static CurrencyManipulator getManipulatorByCurrencyCode(String currencyCode) {
+        String key = currencyCode.toUpperCase();
+        CurrencyManipulator res = map.get(key);
+        if (res == null) {
+            res = new CurrencyManipulator(currencyCode);
+            map.put(key, res);
+        }
+        return res;
+    }
+
+    private CurrencyManipulatorFactory() {}
+}
