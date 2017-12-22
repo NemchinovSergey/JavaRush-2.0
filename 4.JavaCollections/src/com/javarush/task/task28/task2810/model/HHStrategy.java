bcfcc812eb10761ebe1a723876398f4cc96e3946
Sergey Nemchinov <googoodemon@gmail.com>
2017-12-22 23:26:22 +0700

    task2810 (4) done

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task28.task2810.model;
 
-/**  Парсит http://hh.ru  */
+/**  Парсит http://hh.ru
+     https://novosibirsk.hh.ru/search/vacancy?text=java+Новосибирск
+     https://hh.ru/search/vacancy?text=java+ADDITIONAL_VALUE&page=PAGE_VALUE */
 public class HHStrategy implements Strategy {
+    private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
 }
