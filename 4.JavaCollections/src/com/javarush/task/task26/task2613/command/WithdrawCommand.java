d6673b3c531e029332c256ce7fb98a80d45f27f0
Sergey Nemchinov <googoodemon@gmail.com>
2017-12-23 01:10:09 +0700

    task2613 (14) done

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
@@ -1,29 +1,46 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.CashMachine;
 import com.javarush.task.task26.task2613.ConsoleHelper;
 import com.javarush.task.task26.task2613.CurrencyManipulator;
 import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
 import com.javarush.task.task26.task2613.exception.InterruptOperationException;
 import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 
 class WithdrawCommand implements Command {
+    private ResourceBundle res;
+
+    {
+        try (BufferedReader reader = new BufferedReader(new FileReader(CashMachine.RESOURCE_PATH + "withdraw_en.properties"))) {
+            res = new PropertyResourceBundle(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void execute() throws InterruptOperationException {
+        ConsoleHelper.writeMessage(res.getString("before"));
         String currencyCode = ConsoleHelper.askCurrencyCode();
         CurrencyManipulator currencyManipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
         while (true) {
-            ConsoleHelper.writeMessage("Введите сумму:");
+            ConsoleHelper.writeMessage(res.getString("specify.amount"));  //"Введите сумму:"
             try {
                 int userSum = Integer.parseInt(ConsoleHelper.readString());
                 if (userSum <= 0) {
-                    ConsoleHelper.writeMessage("Неправильная сумма (отрицательное число).");
+                    ConsoleHelper.writeMessage(res.getString("specify.not.empty.amount"));  //"Неправильная сумма (0 или отрицательное число)."
                     continue;
                 }
                 if (!currencyManipulator.isAmountAvailable(userSum)) {
-                    ConsoleHelper.writeMessage("Недостаточно средств в банкомате.");
+                    ConsoleHelper.writeMessage(res.getString("not.enough.money"));  //"Недостаточно средств в банкомате."
                     continue;
                 }
 
@@ -39,14 +56,12 @@ class WithdrawCommand implements Command {
                     sb.append("\t").append(nominal).append(" - ").append(value);
                     ConsoleHelper.writeMessage(sb.toString());
                 }
-                ConsoleHelper.writeMessage("Транзакция успешно завершена.");
+                ConsoleHelper.writeMessage(String.format(res.getString("success.format"), userSum, currencyCode));  //"Транзакция успешно завершена."
                 return;
-            }
-            catch (NumberFormatException e) {
-                ConsoleHelper.writeMessage("Ошибка ввода суммы.");
-            }
-            catch (NotEnoughMoneyException e) {
-                ConsoleHelper.writeMessage("Недостаточно купюр для завершения транзакции.");
+            } catch (NumberFormatException e) {
+                ConsoleHelper.writeMessage(res.getString("specify.not.empty.amount"));  //"Ошибка ввода суммы."
+            } catch (NotEnoughMoneyException e) {
+                ConsoleHelper.writeMessage(res.getString("exact.amount.not.available"));  //"Недостаточно купюр для завершения транзакции."
             }
         }
     }
